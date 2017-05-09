5543af731b0b921e962749c0b863c3c12ee7e332
Katie <katiearina@gmail.com>
2017-05-08 20:45:03 -0400

    :v::v::v: Part 1 working properly, need to comment and remove extra console logs. Record video & screen shots.

--- assets/javascript/bamazonCustomer.js
+++ assets/javascript/bamazonCustomer.js
@@ -3,6 +3,9 @@ var inquirer = require("inquirer");
 var prompt = require("prompt");
 var colors = require("colors/safe");
 
+var itemToBuy;
+var itemQuantity;
+
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -36,20 +39,24 @@ function showMeTheItems() {
 				type: "list",
 				message: "Would you like to buy something?",
 				choices: ["Yes, please.",
-					"No, please remind me what you have for sale again."]
+					// "Maybe, please remind me what you have for sale again.",
+					"No, thank you. I'd like to quit for now."]
 			}
 
 				]).then(function(answer) {
 					if (answer.wanttobuy === "Yes, please.") {
-					console.log("Welcome to Bamazon!\n" + 
-						"What item would you like to purchase?");
-					// purchaseSomeStuff();
+					console.log("What item would you like to purchase?");
+					purchaseSomeStuff();
 					}
 
-					else if (answer.wanttobuy === "No, please remind me what you have for sale again.") {
-						showMeTheItems();
+					// else if (answer.wanttobuy === "Maybe, please remind me what you have for sale again.") {
+					// 	showMeTheItems();
+					// }
+
+					else if (answer.wanttobuy === "No, thank you. I'd like to quit for now.") {
+						connection.end();
+						console.log("Thank you for shopping!");
 					}
-					purchaseSomeStuff();
 				});
 			});
 };
@@ -58,7 +65,7 @@ function purchaseSomeStuff() {
 	inquirer.prompt([
 	{
 		name: "itemtobuy",
-		message: "Please enter the ID number of the item you would like to purchase"
+		message: "What is the ID number of the item you'd like to purchase?"
 	},
 	{
 		name: "itemquantity",
@@ -66,74 +73,62 @@ function purchaseSomeStuff() {
 	}
 
 	]).then(function(answer) {
-		connection.query("SELECT * FROM products",
-			function(err, res) {
-				if (err) throw err;
-		// switch (parseInt(answer.itemtobuy)) {
-		// 	case 1:
-		// 		console.log(res[0].product_name);
-		// 		howManyToBuy();
-		// 		break;
-		// 	case 2:
-		// 		output += 'What ';
-		// 		output += 'Is ';
-		// 		break;
-		// 	case 3:
-		// 		output += 'Your ';
-		// 		break;
-		// 	case 4:
-		// 		output += 'Name';
-		// 		break;
-		// 	case 5:
-		// 		output += '?';
-		// 		console.log(output);
-		// 		break;
-		// 	case 6:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 7:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 8:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 9:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 10:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 11:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 12:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 13:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	case 14:
-		// 		output += '!';
-		// 		console.log(output);
-		// 		break;
-		// 	default:
-		// 		console.log("Please enter an Item ID between 1 and 14.");
-		}
+		connection.query("SELECT * FROM products WHERE ?", {
+			item_id: answer.itemtobuy
+		}, function(err, res) {
+			if (err) throw err;
+			// Update validation to tie in to check against item_id more specifically.
+			var intItemToBuy = parseInt(answer.itemtobuy);
+			if ((intItemToBuy < 1) || (intItemToBuy > 14) || (typeof intItemToBuy !== "number")) {
+				console.log(answer.itemtobuy);
+				console.log("That is not a valid item ID, please try again.");
+				purchaseSomeStuff();
+			}
+
+			// Validate quantity ordered to confirm type is number
+			else if (res[0].stock_quantity < answer.itemquantity) {
+				console.log("Insufficient quantity available! Please try again.");
+				purchaseSomeStuff();
+			}
+
+			else if (res[0].stock_quantity >= answer.itemquantity) {
+				console.log("Cool!");
+				itemToBuy = answer.itemtobuy;
+				itemQuantity = answer.itemquantity;
+				updateDatabase();
+			}
 	});
 });
-
 };
 
-function howManyToBuy() {
-	console.log("DOLLA DOLLA BILLZ Y'ALL!");
+function updateDatabase() {
+	console.log(itemToBuy);
+	console.log(itemQuantity);
+	connection.query("SELECT * FROM products WHERE ?", {
+		item_id: itemToBuy
+	}, function (err, res) {
+		if (err) throw err;
+		var newQuantity = (res[0].stock_quantity - itemQuantity);
+		console.log(newQuantity);
+		connection.query("UPDATE products SET ? WHERE ?", [{
+			stock_quantity: newQuantity
+		}, {
+			item_id: itemToBuy
+		}], function (err, res) {
+			if (err) throw err;
+			totalOrderAmount();
+		});
+});
+};
+
+function totalOrderAmount() {
+	connection.query("SELECT price FROM products WHERE ?", {
+		item_id: itemToBuy
+	}, function (err, res) {
+		if (err) throw err;
+		var orderTotal = (res[0].price * itemQuantity);
+		console.log("Your order total is $" + orderTotal.toFixed(2) + "!");
+	});
 };
 
 showMeTheItems();
