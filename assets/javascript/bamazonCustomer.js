6c3e0d626c49998ae25cc70a7c8417a8caf4b1d8
Katie <katiearina@gmail.com>
2017-05-10 20:25:03 -0400

    :construction_worker::hatching_chick: Used prompt for better validation for item ID and quantity. Broke code out into a few more functions for broader functionality.

--- assets/javascript/bamazonCustomer.js
+++ assets/javascript/bamazonCustomer.js
@@ -1,3 +1,6 @@
+//---------------------------------------------------------------------------
+// VARIABLE DECLARATIONS!
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var prompt = require("prompt");
@@ -19,92 +22,130 @@ connection.connect(function(err) {
 	// console.log("connected as id " + connection.threadId);
 });
 
+
+//---------------------------------------------------------------------------
+// FUNCTION DECLARATIONS!
+
 function showMeTheItems() {
 	console.log("------------------------------");
-		console.log("Welcome to Bamazon!\n" + 
-			"Here are all of the items we have for sale:");
-		connection.query("SELECT * FROM products", 
-			function(err, res) {
-				if (err) throw err;
-				// console.log(res);
-				for (var i = 0; i < res.length; i++) {
-				console.log("Item #" + res[i].item_id + ") " + res[i].product_name + " - $" + res[i].price.toFixed(2));
-			}
+	console.log("Welcome to Bamazon!\n" + 
+		"Here are all of the items we have for sale:");
+	connection.query("SELECT * FROM products", 
+		function(err, res) {
+			if (err) throw err;
+			// console.log(res);
+			for (var i = 0; i < res.length; i++) {
+			console.log("Item #" + res[i].item_id + ") " + res[i].product_name + " - $" + res[i].price.toFixed(2));
+		}
 
-			console.log("------------------------------");
-
-			inquirer.prompt([
-			{
-				name: "wanttobuy",
-				type: "list",
-				message: "Would you like to buy something?",
-				choices: ["Yes, please.",
-					// "Maybe, please remind me what you have for sale again.",
-					"No, thank you. I'd like to quit for now."]
-			}
-
-				]).then(function(answer) {
-					if (answer.wanttobuy === "Yes, please.") {
-					console.log("What item would you like to purchase?");
-					purchaseSomeStuff();
-					}
-
-					// else if (answer.wanttobuy === "Maybe, please remind me what you have for sale again.") {
-					// 	showMeTheItems();
-					// }
-
-					else if (answer.wanttobuy === "No, thank you. I'd like to quit for now.") {
-						connection.end();
-						console.log("Thank you for shopping!");
-					}
-				});
-			});
+		console.log("------------------------------");
+		wannaBuy();
+});
 };
 
-function purchaseSomeStuff() {
+
+function wannaBuy() {
 	inquirer.prompt([
 	{
-		name: "itemtobuy",
-		message: "Please enter the item ID number:"
-	},
-	{
-		name: "itemquantity",
-		message: "How many would you like?"
+		name: "wanttobuy",
+		type: "list",
+		message: "Would you like to buy something?",
+		choices: ["Yes, please.",
+			"No, thank you. I'd like to quit for now."]
 	}
 
-	]).then(function(answer) {
+		]).then(function(answer) {
+			if (answer.wanttobuy === "Yes, please.") {
+			purchaseSomeStuff();
+			}
+
+			else if (answer.wanttobuy === "No, thank you. I'd like to quit for now.") {
+				endConnection();
+			}
+		});
+};
+
+
+function wannaBuyAgain() {
+	inquirer.prompt([
+	{
+		name: "wanttobuy",
+		type: "list",
+		message: "Would you like to buy something else?",
+		choices: ["Yes, please.",
+			"Maybe, please remind me what you have for sale again.",
+			"No, thank you. I'd like to quit for now."]
+	}
+
+		]).then(function(answer) {
+			if (answer.wanttobuy === "Yes, please.") {
+			purchaseSomeStuff();
+			}
+
+			else if (answer.wanttobuy === "Maybe, please remind me what you have for sale again.") {
+				showMeTheItems();
+			}
+
+			else if (answer.wanttobuy === "No, thank you. I'd like to quit for now.") {
+				endConnection();
+			}
+		});
+};
+
+
+function purchaseSomeStuff() {
+	console.log("What item would you like to purchase?");
+
+	var purchaseSchema = {
+		properties: {
+			itemtobuy: {
+				description: "Item ID",
+				pattern: /^\d+$/,
+				message: "Please enter a valid number",
+				required: true
+			},
+			itemquantity: {
+				description: "Quantity",
+				pattern: /^\d+$/,
+				message: "Please enter a valid number",
+				required: true	
+			}
+		}
+	};
+
+	prompt.message = ("");
+
+	prompt.start();
+
+	prompt.get(purchaseSchema, function (err, result) {
 		connection.query("SELECT * FROM products WHERE ?", {
-			item_id: answer.itemtobuy
+			item_id: result.itemtobuy
 		}, function(err, res) {
 			if (err) throw err;
-			// Update validation to tie in to check against item_id more specifically.
-			// I don't think this validation is working consistently.
-			var intItemToBuy = parseInt(answer.itemtobuy);
-			if ((intItemToBuy < 1) || (intItemToBuy > 14) || (typeof intItemToBuy !== "number")) {
-				// console.log(answer.itemtobuy);
-				console.log(answer.itemtobuy.toUpperCase() + " is not a valid item ID, please try again.");
+			// Eventually update validation to tie in to check against item_id more specifically.
+			var intItemToBuy = parseInt(result.itemtobuy);
+			if ((intItemToBuy < 1) || (intItemToBuy > 14)) {
+				console.log(result.itemtobuy.toUpperCase() + " is not a valid item ID, please try again.");
 				purchaseSomeStuff();
 			}
 
-			// Validate quantity ordered to confirm type is number
-			else if (res[0].stock_quantity < answer.itemquantity) {
+			else if (res[0].stock_quantity < result.itemquantity) {
 				console.log("Insufficient quantity available! Please try again.");
 				purchaseSomeStuff();
 			}
 
-			else if (res[0].stock_quantity >= answer.itemquantity) {
-				// console.log("Cool!");
-				itemToBuy = answer.itemtobuy;
-				itemQuantity = answer.itemquantity;
+			else if (res[0].stock_quantity >= result.itemquantity) {
+				itemToBuy = result.itemtobuy;
+				itemQuantity = result.itemquantity;
 				updateDatabase();
 			}
 	});
-});
+	});
+
 };
 
+
 function updateDatabase() {
-	// console.log(itemToBuy);
-	// console.log(itemQuantity);
 	connection.query("SELECT * FROM products WHERE ?", {
 		item_id: itemToBuy
 	}, function (err, res) {
@@ -122,6 +163,7 @@ function updateDatabase() {
 });
 };
 
+
 function totalOrderAmount() {
 	connection.query("SELECT * FROM products WHERE ?", {
 		item_id: itemToBuy
@@ -129,7 +171,19 @@ function totalOrderAmount() {
 		if (err) throw err;
 		var orderTotal = (res[0].price * itemQuantity);
 		console.log("You have ordered " + itemQuantity + " of " + res[0].product_name + ".\nYour order total is $" + orderTotal.toFixed(2) + "!");
+		wannaBuyAgain();
 	});
 };
+
+function endConnection() {
+	connection.end();
+	console.log("------------------------------\n" + 
+		"Thank you for shopping!" + 
+		"\n------------------------------");
+};
+
+
+//---------------------------------------------------------------------------
+// FUNCTION CALLS!
 
 showMeTheItems();
